2edb32248c448560485cb52bbee408412eba4bde
Alena Nikulina <alenanikulina0@gmail.com>
2019-07-12 17:54:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
+def map
 
+end
+
+map(source_array){|n| n * -1}
+map(source_array){|n| n}
+map(source_array){|n| n * 2}
+map(source_array){|n| n * n}
+
+def reduce
+
+end
+
+reduce(source_array){|memo, n| memo + n}
+reduce(source_array, starting_point){|memo, n| memo + n}
+reduce(source_array){|memo, n| memo && n}
+reduce(source_array){|memo, n| memo || n}
